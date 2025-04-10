be43f28c7181a934569a31d5c3536f2ef01c9616
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-04-10 14:51:31 -0400

    Entire diamond code update before debugging

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -4,12 +4,91 @@ public class Diamond {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
 
+    System.out.print("Please type in a number to create a diamond:");
     int userInput = scan.nextInt;
 
-    System.out.println(userInput/2);
-    // if (userInput % 2 == 1){
-    //   for (int i = 0; i < (userInput/2); i++)
-    // }
+    //Choice for odd numbers
+    if (userInput % 2 == 1){
+      int spaceAmount = userInput/2;
+
+      //prints the first half of the diamond
+      for(int i = 1; i <= userInput; i+=2){
+        //print out the right amount of spaces
+        for (int j = spaceAmount; j > 0; j--){
+          System.out.print(" ");
+        }
+        spaceAmount--;
+        //print out the correct amount of symbols
+        for(int k = 0; k < i; k++){
+          System.out.print("*");
+        }
+        System.out.print("\n");
+      }
+      spaceAmount++;
+
+      //prints the second half of the diamond
+      for(int i = userInput-2; i > 0; i-=2){
+        //print out the right amount of spaces
+        for (int j = spaceAmount; j > 0; j--){
+          System.out.print(" ");
+        }
+        spaceAmount++;
+        //print out the correct amount of symbols
+        for(int k = 0; k < i; k++){
+          System.out.print("*");
+        }
+        System.out.print("\n");
+      }
+    }
+    //choice for even numbers
+    else{
+      int spaceAmount = userInput-1;
+
+      //prints the first star
+      for(int i = spaceAmount; i > 0; i--){
+        System.out.print(" ");
+      }
+      System.out.print("*");
+      spaceAmount--;
+
+      //prints the rest of the first half of the diamond
+      for(int i = 2; i <= userInput; i+=2){
+        //print out the right amount of spaces
+        for (int j = spaceAmount; j > 0; j--){
+          System.out.print(" ");
+        }
+        spaceAmount -= 2;
+        //print out the correct amount of symbols
+        for(int k = 0; k < i; k++){
+          System.out.print("* ");
+        }
+        System.out.print("\n");
+      }
+      spaceAmount += 2;
+
+      //prints the second half of the diamond without the last star
+      for(int i = userInput-2; i > 0; i-=2){
+        //print out the right amount of spaces
+        for (int j = spaceAmount; j > 0; j--){
+          System.out.print(" ");
+        }
+        spaceAmount += 2;
+        //print out the correct amount of symbols
+        for(int k = 0; k < i; k++){
+          System.out.print("* ");
+        }
+        System.out.print("\n");
+      }
+
+      //prints the last star
+      spaceAmount++;
+      for(int i = spaceAmount; i > 0; i--){
+        System.out.print(" ");
+      }
+      System.out.print("*");
+
+
+    }
 
   }
 }
