751b3ec19ca8a002fa5f434a2941ac9b5af15764
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-04-10 11:54:32 -0400

    Troubleshooting makefile

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int userInput = scan.nextInt;
+
+    System.out.println(userInput/2);
+    // if (userInput % 2 == 1){
+    //   for (int i = 0; i < (userInput/2); i++)
+    // }
+
   }
 }
